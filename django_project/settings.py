8bad02c938f864bae6596db502773993a6e23a77
BrendonLH <Brendonhampton1988@gmail.com>
2022-08-20 20:18:51 -0700

    got the home for ellie may the corgi linked up and running properly

--- django_project/settings.py
+++ django_project/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     # Local
     "accounts",
     "pages",
+    'ellie',
 ]
 
 # MIDDLEWARE
--- django_project/urls.py
+++ django_project/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
     path("", include("pages.urls")),
+    path("ellie", include("ellie.urls")),
 ]
 
 if settings.DEBUG:
