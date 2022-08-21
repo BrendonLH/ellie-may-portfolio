e42cfeb83d71191ba435cc9059d9bdbc1098fc88
BrendonLH <Brendonhampton1988@gmail.com>
2022-08-20 20:56:03 -0700

    got the url patterns working for going form ellie home to ellie facts

--- ellie/urls.py
+++ ellie/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import HomePageView
+from .views import HomePageView, FactDetailView, FactListView
 
 urlpatterns = [
-    path("", HomePageView.as_view(), name='ellies_home')
+    path("", HomePageView.as_view(), name='ellies_home'),
+    path("facts", FactListView.as_view(), name='facts'),
+    path('<int:pk>/', FactDetailView.as_view(), name='fact_detail'),
 ]
--- ellie/views.py
+++ ellie/views.py
@@ -1,7 +1,19 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, DetailView, ListView
+
+from .models import Fact
 
 
 # Create your views here.
 
 class HomePageView(TemplateView):
     template_name = 'pages/ellie_home.html'
+
+
+class FactListView(ListView):
+    template_name = 'pages/ellie_fact_list.html'
+    model = Fact
+
+
+class FactDetailView(DetailView):
+    model = Fact
+    template_name = 'pages/ellie_fact_detail.html'
